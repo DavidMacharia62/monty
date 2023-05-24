9bee79b1b2ce59c5dfddc2ced59b0301d7bd20dc
DavidMacharia62 <davidmacharia1@gmail.com>
2023-05-24 11:33:05 +0300

    monty

---
+++ sub.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+/**
+ * sub - Subtracts the top element of the stack from the second top element.
+ * @stack: Double pointer to the stack.
+ * @line_number: Line number in the file.
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    (*stack)->next->n -= (*stack)->n;
+    pop(stack, line_number);
+}
